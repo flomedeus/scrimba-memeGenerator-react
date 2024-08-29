bd15a4428ed2b4894c13d361dc5825598174077f
flomedeus <cristialt2727@gmail.com>
2024-08-29 18:54:56 +0300

    added files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header.jsx"
+import Meme from "./Meme.jsx"
+
+export default function App() {
+  return (
+      <div className="wrapper">
+          <Header />
+          <Meme />
+      </div>
+  )
+}
